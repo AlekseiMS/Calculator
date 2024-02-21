97ad1875d9f60d883c9d213b2742343347bb97c0
Aleksei Samoilov <alekseisamoilov064@gmail.com>
2024-02-21 11:50:58 +0400

    Adding the remaining files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.itsamoilov_calc {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.itsamoilov_calc to javafx.fxml;
     exports com.example.itsamoilov_calc;
 }
